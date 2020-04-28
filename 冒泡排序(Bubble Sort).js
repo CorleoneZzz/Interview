34b4e8e6099c8109e1e9d478a8b36534e2418258
Guobinze <1410131029@qq.com>
2020-04-28 11:57:32 +0800

    add sort

--- 冒泡排序(Bubble Sort).js
+++ 冒泡排序(Bubble Sort).js
@@ -18,12 +18,12 @@ function bubbleSort(arr){
 
 function bubbleSort(arr){
     for(let i = 0; i < arr.length; i++) {
-        let flag = true
+        let flag = true;
         for(let j = 0; j < arr.length - i - 1; j++) {
             if(arr[j] > arr[j+1]) {
-                flag = false
-                let temp = arr[j]
-                arr[j] = arr[j+1]
+                flag = false;
+                let temp = arr[j];
+                arr[j] = arr[j+1];
                 arr[j+1] = temp
             }
         }
