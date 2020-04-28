72dc448fcb79e754d645c6ee8d17ce6ce48eaaf2
Guobinze <1410131029@qq.com>
2020-04-28 12:16:06 +0800

    add sort

--- 冒泡排序(Bubble Sort).js
+++ 冒泡排序(Bubble Sort).js
@@ -1,8 +1,9 @@
 // 冒泡排序（Bubble Sort）也是一种简单直观的排序算法。它重复地走访过要排序的数列，一次比较两个元素，如果他们的顺序错误就把他们交换过来。
 // 走访数列的工作是重复地进行直到没有再需要交换，也就是说该数列已经排序完成。这个算法的名字由来是因为越小的元素会经由交换慢慢"浮"到数列的顶端。
 function bubbleSort(arr){
-    for(let i = 0; i < arr.length; i++) {
-        for(let j = 0; j < arr.length - i - 1; j++) {
+    const len = arr.length;
+    for(let i = 0; i < len - 1; i++) {
+        for(let j = 0; j < len - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 let temp = arr[j];
                 arr[j] = arr[j + 1];
@@ -17,9 +18,10 @@ function bubbleSort(arr){
 // 为了避免这种情况，我们增加一个flag，判断排序是否在中途就已经完成（也就是判断有无发生元素交换）
 
 function bubbleSort(arr){
-    for(let i = 0; i < arr.length; i++) {
+    const len = arr.length;
+    for(let i = 0; i < len - 1; i++) {
         let flag = true;
-        for(let j = 0; j < arr.length - i - 1; j++) {
+        for(let j = 0; j < len - i - 1; j++) {
             if(arr[j] > arr[j + 1]) {
                 flag = false;
                 let temp = arr[j];
--- 插入排序(Insertion Sort).js
+++ 插入排序(Insertion Sort).js
@@ -3,7 +3,7 @@
 // 插入排序在实现上，通常采用in-place排序（即只需用到O(1)的额外空间的排序），因而在从后向前扫描过程中，需要反复把已排序元素逐步向后挪位，为最新元素提供插入空间。
 function insertSort(arr) {
     const len = arr.length;
-    for (let i = 1; i < len; i++) {
+    for (let i = 0; i < len - 1; i++) {
         for (let j = i; j > 0; j--) {
             if (arr[j] < arr[j - 1]) {
                 let temp = arr[j];
