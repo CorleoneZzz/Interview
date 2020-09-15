b31bf06d60306ba6d383509c73a647e9cc00a72a
Guobinze <1410131029@qq.com>
2020-09-15 10:52:04 +0800

    promise

--- JavaScript/手写promise.js
+++ JavaScript/手写promise.js
@@ -1,3 +1,90 @@
+// promise的使用
+function runAsync1(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务1执行完成');
+            resolve('随便什么数据1');
+        }, 1000);
+    });
+    return p;
+}
+function runAsync2(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务2执行完成');
+            resolve('随便什么数据2');
+        }, 2000);
+    });
+    return p;
+}
+function runAsync3(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务3执行完成');
+            resolve('随便什么数据3');
+        }, 2000);
+    });
+    return p;
+}
+runAsync1()
+    .then(function(data){
+        console.log(data);
+        console.log(a)
+        return runAsync2();
+    })
+    .catch(function(data){
+        console.log(data);
+        return runAsync3();
+    })
+    .then(function(data){
+        console.log(data);
+    });    function runAsync1(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务1执行完成');
+            resolve('随便什么数据1');
+        }, 1000);
+    });
+    return p;
+}
+function runAsync2(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务2执行完成');
+            resolve('随便什么数据2');
+        }, 2000);
+    });
+    return p;
+}
+function runAsync3(){
+    var p = new Promise(function(resolve, reject){
+        //做一些异步操作
+        setTimeout(function(){
+            console.log('异步任务3执行完成');
+            resolve('随便什么数据3');
+        }, 2000);
+    });
+    return p;
+}
+runAsync1()
+    .then(function(data){
+        console.log(data);
+        console.log(a)
+        return runAsync2();
+    })
+    .catch(function(data){
+        console.log(data);
+        return runAsync3();
+    })
+    .then(function(data){
+        console.log(data);
+    });
+// 手写promise
 function Promise(executor) {
     this.state = 'pending'; //状态
     this.value = undefined; //成功结果
